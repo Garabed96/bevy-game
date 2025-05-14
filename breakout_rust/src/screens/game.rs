4b67eeae865537dccc23e6ce8e1ab90618f2043d
Garo <garonazarian09@gmail.com>
2025-05-14 16:51:47 +0700

    - character rust file (moved functionality for animation)

--- breakout_rust/src/screens/game.rs
+++ breakout_rust/src/screens/game.rs
@@ -145,6 +145,7 @@ enum WallLocation {
     Top,
 }
 
+use crate::screens::character::{animate_sprite, setup_texture_character};
 use crate::{despawn_screen, DisplayQuality, GameState, Volume};
 use bevy::{
     color::palettes::basic::{BLUE, LIME},
@@ -408,106 +409,6 @@ fn game(
 ) {
     if timer.tick(time.delta()).finished() {
         game_state.set(GameState::Menu);
-    }
-}
-
-struct SpriteSheet {
-    size: Vec2,
-    text: String,
-    transform: Transform,
-    texture: Handle<Image>,
-    image_mode: SpriteImageMode,
-    atlas: TextureAtlas,
-    indices: AnimationIndices,
-    timer: AnimationTimer,
-}
-
-fn setup_texture_character(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    // CHARACTER COMPONENTS
-
-    let character: Handle<Image> = asset_server.load("textures/player_idle_sheet.png");
-    let animation_indices_character = AnimationIndices { first: 0, last: 7 };
-    let character_atlas = TextureAtlas {
-        layout: texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
-            UVec2::new(300, 256),
-            8,
-            1,
-            None,
-            None,
-        )),
-        index: animation_indices_character.first,
-    };
-
-    // TODO: Add ability to name your character, save in json for now
-    let sprint_sheets = [SpriteSheet {
-        size: Vec2::new(300., 256.),
-        text: "Champ Champ".to_string(),
-        transform: Transform {
-            translation: Vec3::new(1. * 300. * 0.25, 0.0, 0.0),
-            ..Transform::from_scale(Vec3::splat(0.25))
-        },
-        texture: character.clone(),
-        image_mode: SpriteImageMode::Auto,
-        atlas: character_atlas.clone(),
-        indices: animation_indices_character.clone(),
-        timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-    }];
-
-    for sprite_sheet in sprint_sheets {
-        let mut cmd = commands.spawn((
-            Sprite {
-                image_mode: sprite_sheet.image_mode,
-                custom_size: Some(sprite_sheet.size),
-                ..Sprite::from_atlas_image(sprite_sheet.texture.clone(), sprite_sheet.atlas.clone())
-            },
-            sprite_sheet.indices,
-            sprite_sheet.timer,
-            sprite_sheet.transform,
-        ));
-
-        cmd.with_children(|builder| {
-            builder.spawn((
-                Text2d::new(sprite_sheet.text),
-                TextLayout::new_with_justify(JustifyText::Center),
-                TextColor(TEXT_COLOR),
-                TextFont::from_font_size(55.),
-                Transform::from_xyz(0., -256. * 0.5 - 10., 0.),
-                bevy::sprite::Anchor::TopCenter,
-            ));
-        });
-    }
-}
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-// Setup the character
-#[derive(Component, Clone)]
-struct AnimationIndices {
-    first: usize,
-    last: usize,
-}
-
-fn animate_sprite(
-    time: Res<Time>,
-    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut Sprite)>,
-) {
-    for (indices, mut timer, mut sprite) in &mut query {
-        timer.tick(time.delta());
-
-        if timer.just_finished() {
-            if let Some(atlas) = &mut sprite.texture_atlas {
-                atlas.index = if atlas.index == indices.last {
-                    indices.first
-                } else {
-                    atlas.index + 1
-                };
-            }
-        }
     }
 }
 
--- breakout_rust/src/screens/mod.rs
+++ breakout_rust/src/screens/mod.rs
@@ -1,3 +1,4 @@
 pub mod game;
 pub mod menu;
 pub mod splash;
+pub mod character;
